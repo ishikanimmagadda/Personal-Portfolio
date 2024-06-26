2aadf7201b1caedc26d4abac286c2e8bd2b4af1b
ishikanimmagadda <ishikan84@gmail.com>
2024-06-26 15:01:55 -0500

    db working, nav bar done, made a little prettier

--- main.py
+++ main.py
@@ -1,42 +1,51 @@
+
 from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__) 
-    
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contacts.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
 @app.route("/") 
 def index(): 
-    return "Homepage for Ishika"
+    return render_template('home.html')
 
 @app.route("/about") 
 def about(): 
-    return "Hello, Welcome to the About Page"
+    return render_template('about.html')
   
 
 @app.route("/projects") 
 def projects(): 
-    return "Hello, Welcome to the Projects Page"
+    return render_template('projects.html')
   
 
 @app.route("/contact", methods =["GET"] ) 
 def contact(): 
-    if request.method == 'GET':
-        if(request.args.get('email') == None):
-            return render_template('contact.html')
-        elif(request.args.get('num') == ''):
-            return "<html><body> <h1>Invalid email</h1></body></html>"
-        else:
-            email = request.args.get('email')
-            return render_template('answer.html', 
-                                   email = email)
-
+    return render_template('contact.html')
 
 @app.route("/submit_contact", methods=["POST"])
 def submit_contact():
     email = request.form.get('email')
     if email:
-        return f"Hello, Welcome to the Contact Page. We will reach out to {email} soon!"
+        new_contact = Contact(email=email)
+        try: 
+            db.session.add(new_contact)
+            db.session.commit()
+            return f"Hello, Welcome to the Contact Page. I will reach out to {email} soon!"
+        except IntegrityError:
+            db.session.rollback()
+            return f"This email {email} has already been added!"
     else:
         return "No email provided."
-    
 
 if __name__ == "__main__": 
-    app.run(debug=True) 
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)     
